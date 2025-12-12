c5f398191fc25e932cdb30752ea90ccefb452f19
EmilyWatkinz <sewatkins16@gmail.com>
2025-12-12 13:06:59 -0800

    Nav ba and highlights

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Landing from './components/landing.jsx';
 import Nav from './components/nav.jsx'
+import Highlights from './components/highlights.jsx';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
+      <Highlights />
     </div>
   );
 }
